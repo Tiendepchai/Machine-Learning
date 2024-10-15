41e2e6f5b5eb4c02efecce84b04f62133767a234
Tiendepchai <chodienkk@icloud.com>
2024-10-15 13:50:36 +0700

    DONE ID3

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 from model import *
+import pandas as pd
 
-def cal_distance(A: list[int], B: list[int]):
-  return ((A[0]-B[0])**2 + (A[1]-B[1])**2)**(1/2)
-
-if __name__  == "__main__":
+def Assignment_1():
   data: list[list[int]] = [[2, 4], [4, 6], [4, 2], [6, 4], [6, 6]]
   label = ['Red', 'Red', 'Blue', 'Blue', 'Red']
   d_new = [5, 5]
   centroid = Centroids_2D(data, label)
   print(centroid.predict(d_new))
 
-  
+def Assignment_2():
+  df = pd.read_csv('Data/data.csv')
+  data, label = df.iloc[:, 0:4], df.iloc[:, 4] 
+  model = ID3().fit(data, label)
+
+if __name__ == "__main__":
+  Assignment_2()
--- model.py
+++ model.py
@@ -1,5 +1,8 @@
 from queue import PriorityQueue
 from collections import defaultdict
+from math import *
+import pandas as pd
+import numpy as np
 class KNN_2D:
   def __init__(self, data: list[list[int]], label: list[any]) -> None:
     self.data = data
@@ -60,3 +63,69 @@ class Centroids_2D:
     
     return ans.get()[1]
 
+class ID3:
+  def __init__(self) -> None:
+    self.data = None
+    self.label = None
+    self.entropy = 0
+    self.features = None
+    self.IGs = list()
+    self.greatest_IG = None
+
+  def calculate_entropy(self, data, label:pd.DataFrame):
+    total_rows = len(data)
+    count = label.value_counts()
+    target_values = label.values
+    target_values = np.unique(target_values)
+
+    entropy = 0
+    for value in target_values: 
+      pi = count[value]
+      proportion = pi / total_rows
+      entropy += (- proportion * log2(proportion))
+    return entropy
+
+  def __cal_IG(self):
+    for i in self.features:
+      self.IGs.append((self.entropy - self.features[i], i))
+    self.IGs.sort()
+    self.greatest_IG = self.IGs[-1]
+
+  def cal_H_features(self, data:pd.DataFrame, label:pd.DataFrame):
+    title = data.columns.values
+    ans = {}
+    lbl = label.values
+    target_values = np.unique(label.values)
+    for i in title:
+      sett = np.unique(data[i].values)
+      cnt_vals = data[i].value_counts()
+      arr = data[i].to_list()
+      l = len(arr)
+      IG = 0
+      for x in sett:
+        cc = 0
+        cnt = {x: 0 for x in target_values}
+        for y in range(l):
+          if arr[y] == x:
+            cnt[lbl[y]] += 1
+            cc += 1
+        # Calculate entropy
+        
+        entropy = 0
+        for value in cnt: 
+          pi = cnt[value]
+          if pi != 0:
+            proportion = pi / cc
+            entropy += (- proportion * log2(proportion))
+          
+        IG += (cnt_vals[x]/l)*entropy
+      ans[i] = IG
+    return ans
+
+  def fit(self, data, label):
+    self.data = data
+    self.label = label
+    self.entropy = self.calculate_entropy(data, label)
+    self.features = self.cal_H_features(data, label)
+    self.__cal_IG()
+    print(self.greatest_IG)
